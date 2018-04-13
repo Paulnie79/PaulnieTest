90cfdd6400ad335500e0a3e10fa1f13cbedf6d31
Paulnie79 <nie_gl123@163.com>
2018-04-13 16:53:45 +0800

    second_commit

---
+++ hello/src/main/java/com/didispace/web/HelloController.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.didispace.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author paul
+ *
+ */
+
+@RestController
+@RequestMapping(path = "/v1")
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String index() {
+		return "first hello world!";
+	}
+
+}
---
+++ hello/src/main/java/com/didispace/web/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author paul
+ *
+ */
+package com.didispace.web;
